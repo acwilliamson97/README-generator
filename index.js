ad6b6d6d55fe71dd74a6ff12765c4a5afdde5ec6
Anna Williamson <acwilliamson97@gmail.com>
2020-06-04 22:26:48 -0700

    test app

--- index.js
+++ index.js
@@ -1,12 +1,78 @@
-const questions = [
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const api = require("./utils/axios.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
+const questions = [{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+},
+{
+    type: "input",
+    message: "What is your email?",
+    name: "email",
+    default: "placeholder@email.com"
+},
+{
+    type: "input",
+    message: "What is the name of your Project?",
+    name: "title",
+    default: "Project Name Here"
+},
+{
+    type: "input",
+    message: "Please briefly describe your project here.",
+    name: "description",
+    default: "Description Here"
+},
+{
+    type: "list",
+    message: "What kind of license should your project have?",
+    name: "license",
+    choices: ["APACHE_2.0", "GPL_3.0", "BSD_3", "MPL_2.0", "None"]
+},
+{
+    type: "input",
+    message: "What command should be used to install dependencies?",
+    default: "npm install",
+    name: "install"
+},
+{
+    type: "input",
+    message: "What command should be used to run tests?",
+    default: "npm test",
+    name: "test"
+},
+{
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "usage",
+    default: "Usage Details Here"
+},
+{
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "contributing",
+    default: "Contributing Details Here"
+},
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+    console.log(`READ-ME created successfully.`);
 }
 
 function init() {
-
-}
+    inquirer.prompt(questions).then(answers => {
+        api.getUser(answers.username).then(response => {
+            console.log(response)
+            const data = {...response, ...answers };
+            console.log(data)
+            writeToFile("./output/README.md", generateMarkdown(data));
+        })
+    })
+};
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,43 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+Install required dependencies using the following command:
+\`\`\`
+${data.install}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## Licensing
+${data.license}
+
+## Contributing
+${data.contributing}
+
+## Testing
+To run tests, use the following command:
+\`\`\`
+${data.test}
+\`\`\`
+
+## Questions
+<img src="${data.avatar_url}" alt="GitHub User Icon" style="border-radius: 30px" width="50">  
+If you have questions about the repo, open an issue or contact [${data.username}](${data.html_url}) directly at ${data.email}.
 `;
 }
 
